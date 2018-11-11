7e149c2bde08009d0f6e10dcc36646b5bd2fed4a
Pawel Maslak <maslak.pawel89@gmail.com>
2018-11-11 11:37:55 +0000

    Final Review + Constraints Removal in Model Imported from Changeset 128

---
+++ Maslshop/Maslshop/Migrations/201811111102477_removalOfConstraints.cs
@@ -0,0 +1,28 @@
+namespace Maslshop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class removalOfConstraints : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AspNetUsers", "Name", c => c.String());
+            AlterColumn("dbo.AspNetUsers", "Surname", c => c.String());
+            AlterColumn("dbo.AspNetUsers", "Address", c => c.String());
+            AlterColumn("dbo.AspNetUsers", "PostCode", c => c.String());
+            AlterColumn("dbo.AspNetUsers", "City", c => c.String());
+            AlterColumn("dbo.AspNetUsers", "RegistrationDate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.AspNetUsers", "RegistrationDate", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.AspNetUsers", "City", c => c.String(nullable: false, maxLength: 100));
+            AlterColumn("dbo.AspNetUsers", "PostCode", c => c.String(nullable: false));
+            AlterColumn("dbo.AspNetUsers", "Address", c => c.String(nullable: false, maxLength: 100));
+            AlterColumn("dbo.AspNetUsers", "Surname", c => c.String(nullable: false, maxLength: 100));
+            AlterColumn("dbo.AspNetUsers", "Name", c => c.String(nullable: false, maxLength: 100));
+        }
+    }
+}
--- Maslshop/Maslshop/Models/Core/ApplicationUser.cs
+++ Maslshop/Maslshop/Models/Core/ApplicationUser.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
-using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -10,26 +9,16 @@ namespace Maslshop.Models.Core
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        [Required]
-        [StringLength(100)]
         public string Name { get; set; }
 
-        [Required]
-        [StringLength(100)]
         public string Surname { get; set; }
 
-        [Required]
-        [StringLength(100)]
         public string Address { get; set; }
 
-        [Required]
         public string PostCode { get; set; }
 
-        [Required]
-        [StringLength(100)]
         public string City { get; set; }
 
-        [Required]
         public virtual DateTime? RegistrationDate { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
